9f027c84e94d49d7815953cdb6761c3423c8e571
bijeaylimbu <bijaylimbu11111@gmail.com>
2021-02-24 14:57:23 +0545

    additional files and changes for Heroku

--- account/settings.py
+++ account/settings.py
@@ -239,7 +239,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
