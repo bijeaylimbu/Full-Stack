ba953ba5bef3403e00a433722d0dddb98d6e4344
bijeaylimbu <bijaylimbu11111@gmail.com>
2021-02-24 11:38:46 +0545

    additional files and changes for Heroku

--- account/settings.py
+++ account/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l5(+-g%nua_^lo*gou%(_9*abqo1%fs%$!(tx5!apizmj5icwu'
 # SECRET_KEY=os.environ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://djangowithreactjs.herokuapp.com']
 
